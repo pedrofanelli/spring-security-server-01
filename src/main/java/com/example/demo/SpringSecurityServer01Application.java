ca35114fb9cff3fef309e46bd699fd0e47dbc295
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-15 19:18:39 -0300

    git init

---
+++ src/main/java/com/example/demo/SpringSecurityServer01Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityServer01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityServer01Application.class, args);
+	}
+	
+	
+
+}
