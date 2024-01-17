94caf03be49aff59cc6cfd71f44da236fddf7049
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-17 19:14:36 -0300

    setting up config

--- src/main/java/com/example/demo/config/ProjectConfig.java
+++ src/main/java/com/example/demo/config/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,6 +13,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class ProjectConfig {
 
+	@Autowired
+    //private AuthenticationProviderService authenticationProvider;
+	
 	@Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         
