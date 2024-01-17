527c5e544ea6512230961af71500d72bdce2254a
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-17 19:23:45 -0300

    building from files

--- src/main/java/com/example/demo/services/CustomAuthProvider.java
+++ src/main/java/com/example/demo/services/CustomAuthProvider.java
@@ -1,21 +1,43 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAuthProvider implements AuthenticationProvider {
 
+	//@Autowired
+    //private JpaUserDetailsService userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	
 	@Override
     public Authentication authenticate (Authentication authentication) throws AuthenticationException {
-        
+		String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        CustomUserDetails user = userDetailsService.loadUserByUsername(username);
+
+        switch (user.getUser().getAlgorithm()) {
+            case BCRYPT:
+                return checkPassword(user, password, bCryptPasswordEncoder);
+            case SCRYPT:
+                return checkPassword(user, password, sCryptPasswordEncoder);
+        }
+
+        throw new  BadCredentialsException("Bad credentials");
     }
     
     @Override
     public boolean supports(Class<?> authenticationType) {
-        
+    	return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authenticationType);
     }
 	
 }
