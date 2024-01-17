31f5dc3e63dcf023bf15949f2693cc80a1646400
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-17 19:09:36 -0300

    building config

--- src/main/java/com/example/demo/config/ProjectConfig.java
+++ src/main/java/com/example/demo/config/ProjectConfig.java
@@ -1,9 +1,26 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class ProjectConfig {
 
+	@Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        
+        http.httpBasic(Customizer.withDefaults());
+        
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated()); //.permitAll()
+                
+        return http.build();
+    }
+	
+	
 	
 }
