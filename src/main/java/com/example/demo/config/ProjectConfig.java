99a66cbfeeee4cc72b135306b56ac1ddc9406d37
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-17 19:13:32 -0300

    encoder

--- src/main/java/com/example/demo/config/ProjectConfig.java
+++ src/main/java/com/example/demo/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,11 +17,17 @@ public class ProjectConfig {
         
         http.httpBasic(Customizer.withDefaults());
         
+        http.authenticationProvider(null);
+        
         http.authorizeHttpRequests(c -> c.anyRequest().authenticated()); //.permitAll()
                 
         return http.build();
     }
 	
+	@Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 	
 	
 }
