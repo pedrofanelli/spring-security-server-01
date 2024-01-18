f305e8c9523920d3b36cea69aac0b642789ff97d
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-18 18:46:44 -0300

    adding filter

---
+++ src/main/java/com/example/demo/filters/Filter01.java
@@ -0,0 +1,31 @@
+package com.example.demo.filters;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Filter01 extends OncePerRequestFilter {
+
+	@Override
+    public void doFilterInternal (HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = request;
+        HttpServletResponse httpResponse = response;
+        String requestId = request.getHeader("Custom-Header");
+        if (requestId == null || requestId.equals("")) {
+            //httpResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            //return;
+            response.setHeader("Custom-Header", "161616");
+            System.out.println("REQUEST ID HEADER ES NULL!!");
+        }
+        
+        System.out.println("SEGUIMOOOOOOOOH");
+        filterChain.doFilter(request, response);
+        
+    }
+}
