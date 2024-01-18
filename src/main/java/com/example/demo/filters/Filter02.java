90b14614fabad5348a728692d6d270d992845530
Pedro Fanelli <pedrofanelli1@gmail.com>
2024-01-18 18:48:23 -0300

    filter 02

---
+++ src/main/java/com/example/demo/filters/Filter02.java
@@ -0,0 +1,53 @@
+package com.example.demo.filters;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Filter02 extends OncePerRequestFilter {
+
+	@Override
+    public void doFilterInternal (HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = request;
+        HttpServletResponse httpResponse = response;
+        String requestId = request.getHeader("Custom-Header");
+        if (requestId == null || requestId.equals("")) {
+            System.out.println("REQUEST ID HEADER ES NULL :(((!!");
+        } else {
+            System.out.println("EL HEADER ID ES: "+requestId);
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            System.out.println("Current User: " + authentication.getName());
+            Object principal = authentication.getPrincipal();
+            String username = "";
+            if (principal instanceof UserDetails) {
+                System.out.println("INSTANCIAAAA");
+                username = ((UserDetails) principal).getUsername();
+            } else {
+                username = principal.toString();
+            }
+            System.out.println("NOMBREEE "+username);
+            
+            
+        } else {
+            System.out.println("No authenticated user");
+        }
+        
+        
+        System.out.println(request.getSession().getId());
+        
+        filterChain.doFilter(request, response);
+        
+    }
+}
